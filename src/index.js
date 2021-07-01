5b490ae75ec0fe24ff7e957d3ae00a2193b2bf33
Dglz94 <dglz94@hotmail.com>
2021-07-01 12:16:10 -0500

    se agrega codigo del proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import ReactDOM from "react-dom";
-import React from "react";
-const App = () => {
-  return <h1>This is my React app!</h1>;
-};
-ReactDOM.render(<App />, document.getElementById("app"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App';
+import { Provider } from 'react-redux';
+import store from './app/store';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('app')
+);
